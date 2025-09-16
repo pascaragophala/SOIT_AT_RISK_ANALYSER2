93de7b7f4574533b0d765d100d5a2009bc31604f
Pasca Ragophala <123935582+pascaragophala@users.noreply.github.com>
2025-09-16 14:42:57 +0200

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from app import app as app  # if your Flask instance is named `app` inside app.py
+
+# If you use an app factory pattern (create_app), uncomment the lines below and delete the line above:
+# from app import create_app
+# app = create_app()
